69e5810df44c2fbe683aa28301eb1dbe5a4b220b
scoiatael <czapl.luk@gmail.com>
2013-04-19 00:47:35 +0100

    Game server and client added

--- sockets/Client.hs
+++ sockets/Client.hs
@@ -4,30 +4,68 @@ import System.Environment (getArgs, getProgName)
 import Control.Monad.Fix (fix)
 import Control.Monad
 import Control.Concurrent
+import Control.Concurrent.MVar
 
 main = withSocketsDo $ do
   args <- getArgs
-  if (length args) < 2 then do { name <- getProgName; printUsage name;} else do 
-    h <- connectTo (args !! 0) (PortNumber $ toEnum (read (args !! 1) :: Int))
+  if (length args) < 3 then do { name <- getProgName; printUsage name;} else do 
+    let hostname = (args !! 1)
+    let port = (PortNumber $ toEnum (read (args !! 2) :: Int))
+    let ctype = args !! 0
+    when ( ctype == "chat") $ startChat hostname port
+    when ( ctype == "game") $ startGame hostname port
+
+startGame = startChat 
+
+startChat hostname port = do
+    h <- connectTo hostname port
     hSetBuffering h NoBuffering
     putStr "\n[q]uit to quit.\n"
-    reader <- forkIO $ mainReaderLoop h
-    mainWriterLoop h
+    sem <- newEmptyMVar
+    reader <- forkIO $ mainReaderLoop h sem
+    mainWriterLoop h sem
+    putStrLn "Bye then."
     killThread reader
     hClose h
 
-mainReaderLoop h = fix $ \loop -> do { ready <- hReady h; if ready then do { cont <- hGetLine h;  putStr (cont++['\n']); hFlush stdout; } else void $ hWaitForInput h 100; loop}
-mainWriterLoop h = fix $ \loop -> 
-  do { 
-    ready <- hWaitForInput stdin (-1); 
-    if ready 
-      then 
-        do { cont <- hGetLine stdin;
-             hPutStr h (cont++['\n']);
-             if ((cont !! 0) == 'q') then putStr "Bye then.\n" else loop; } 
-      else putStr "Timed out.\n"; 
-      }
+mainRWLoop h1 h2 sem = fix $ \loop -> do { 
+  yield;
+  ready <- hWaitForInput h1 100; 
+  when ready $
+   do { 
+      cont <- hGetLine h1; 
+      when (cont == "quit") $ do { void $ tryTakeMVar sem; putMVar sem (); };
+      hPutStrLn h2 cont; 
+      hFlush h2; }; 
+  cont <- isEmptyMVar sem;
+  when cont loop }
 
+mainReaderLoop h sem = mainRWLoop h stdout sem
+mainWriterLoop h sem = mainRWLoop stdin h sem
+
+{--
+mainReaderLoop h sem = fix $ \loop -> do { 
+  yield;
+  ready <- hWaitForInput h 100; 
+  when ready $
+   do { 
+      cont <- hGetLine h; 
+      when (cont == "__quit__") $ do { void $ tryTakeMVar sem; putMVar sem (); };
+      putStrLn cont; 
+      hFlush stdout; } 
+  cont <- isEmptyMVar sem;
+  if cont then loop else putStrLn "Server closed connection."}
+
+mainWriterLoop h sem = fix $ \loop -> do {  
+    yield;
+    ready <- hWaitForInput stdin (100); 
+    when ready $ 
+        do { line <- hGetLine stdin;
+             when (line == "quit") $
+             hPutStrLn h line; }
+      else do { cont <- isEmptyMVar sem; when cont loop;};
+      }
+--}
 {--
 mainWriterLoop h = do
   input <- hReady stdin
@@ -41,4 +79,4 @@ mainWriterLoop h = do
     else do { hWaitForInput stdin 1000; mainLoop h; }
 --}
 
-printUsage name = putStr (name ++ "<hostname> <port>\n")
+printUsage name = putStr (name ++ " { chat | game } <hostname> <port>\n")
--- sockets/Server.hs
+++ sockets/Server.hs
@@ -5,30 +5,93 @@ import System.IO
 import Control.Exception
 import Control.Concurrent
 import Control.Concurrent.Chan
+import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.Fix (fix)
 import System.Environment (getArgs, getProgName)
+import Control.Exception
  
 type Msg = (Int, String)
  
 main :: IO ()
 main = do
-    chan <- newChan
     args <- getArgs
-    when ((length args) < 1) $ do { printUsage; return (); }
+    print args
+    when ((length args) < 2) $ do { printUsage; return (); }
+    let stype = args!!0
+    let portnr = (PortNumber (toEnum (read (args !! 1) :: Int)))
+    when (stype=="chat") $
+        mainChat portnr
+    when (stype=="game") $
+        mainGame portnr
 {--
     sock <- socket AF_INET Stream 0
     setSocketOption sock ReuseAddr 1
     bindSocket sock (SockAddrInet 4242 iNADDR_ANY)
     listen sock 2
 --}
-    sock <- listenOn (PortNumber (toEnum (read (args !! 0) :: Int)))
+
+mainGame port = do
+  putStrLn "Starting game server..."
+  sock <- listenOn port
+  (hdl1, _, _) <- accept sock
+  putStrLn "Got first player"
+  hSetBuffering hdl1 NoBuffering
+  player1 <- loadPlayer hdl1
+  (hdl2, _, _) <- accept sock
+  putStrLn "Got second player"
+  hSetBuffering hdl2 NoBuffering
+  player2 <- loadPlayer hdl2
+  startGame (player1, hdl1) (player2, hdl2)
+
+loadPlayer hdl = return ""
+
+startGame (pl1, hdl1) (pl2, hdl2) = do
+  sem <- newEmptyMVar
+  thr1 <- simplicityWrap (pl1, hdl1) hdl2 sem "Player1"
+  putStrLn ("Player1: " ++ (show thr1))
+  thr2 <- simplicityWrap (pl2, hdl2) hdl1 sem "Player2"
+  putStrLn ("Player2: " ++ (show thr2))
+  void $ takeMVar sem
+  putStrLn "Ending game.."
+  endConnection hdl1;
+  endConnection hdl2;
+  safeKillThread thr1
+  safeKillThread thr2
+  where
+    simplicityWrap phdl hdl sem pl = 
+      forkIO $ do {forwardInfo phdl hdl sem; putStrLn (pl ++ " left"); putMVar sem();} `catch` exceptionHandler
+    safeKillThread tid = do { putStrLn ("Killing " ++ (show tid)); killThread tid;}
+
+exceptionHandler :: SomeException -> IO ()
+exceptionHandler e = do {putStr "Error ignored: "; print $ toException e;}
+
+forwardInfo (fpl, fhdl) thdl sem = do
+  hPutStrLn fhdl "ping"
+  move <- hGetLine fhdl
+  putStrLn move 
+  let (fpl', okmove) = validMove fpl move
+  when okmove $ hPutStrLn thdl move
+  when (not okmove) $ hPutStrLn fhdl "bad move"
+  when (move /= "quit") $ forwardInfo (fpl', fhdl) thdl sem
+
+endConnection hdl = do
+  putStrLn "Quitting connection..."
+  isOpen <- hIsOpen hdl
+  when isOpen $ do { hPutStrLn hdl "__quit__"; putStrLn "sent goodbyes..";  hClose hdl} `catch` exceptionHandler
+
+validMove _ _ = ("", True)
+  
+mainChat port = do
+    chan <- newChan
+    sock <- listenOn port
     forkIO $ fix $ \loop -> do
         (_, msg) <- readChan chan
+        putStrLn msg
         loop
     mainLoop sock chan 0
  
-printUsage = do { prog <- getProgName; putStr ("Usage: " ++ prog ++ " <port>\n"); }
+printUsage = do { prog <- getProgName; putStr ("Usage: " ++ prog ++ "{ chat | game } <port>\n"); }
 
 mainLoop :: Socket -> Chan Msg -> Int -> IO ()
 mainLoop sock chan nr = do
@@ -38,7 +101,7 @@ mainLoop sock chan nr = do
  
 runConn :: (Handle, HostName, PortNumber) -> Chan Msg -> Int -> IO ()
 runConn (hdl, _, _) chan nr = do
-    let broadcast msg = do { writeChan chan (nr, msg); putStr (msg++"\n"); }
+    let broadcast msg = do { writeChan chan (nr, msg) }
     broadcast ("New connection. " ++ show nr)
     hSetBuffering hdl NoBuffering
     hPutStr hdl "Hi, what's your name?\n"
